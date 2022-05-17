a5b25a94bf53e1be1d5eeaebf523cabb88f2350b
alejandro_diaz <diazbrandonalejandro@gmail.com>
2022-05-17 15:35:21 +0200

    switch de piedra papel tijera

--- js/switch.js
+++ js/switch.js
@@ -81,3 +81,41 @@ switch (expression) {
   }
       console.log(`${fruit} is type of: ${type}.`)
   
+// ejercicio piedra papel tijera
+let rock = 'rock';
+let scissors = 'scissors';
+let paper = 'paper';
+
+// these two variables will be changed to test the conditional
+let firstPlayerChoice = rock
+let secondPlayerChoice = 123
+
+let firstPossibility = firstPlayerChoice === paper && secondPlayerChoice === rock;
+let secondPossibility = firstPlayerChoice === rock && secondPlayerChoice === scissors;
+let thirdPossibility = firstPlayerChoice === scissors && secondPlayerChoice === paper;
+
+let fourthPossibility = secondPlayerChoice === rock && firstPlayerChoice === scissors
+let fifthPossibility = secondPlayerChoice === paper && firstPlayerChoice === rock
+let sixthPossibility = secondPlayerChoice === scissors && firstPlayerChoice === paper
+
+let seventhPossibility = firstPlayerChoice === secondPlayerChoice;
+
+
+
+if (firstPossibility ||
+    secondPossibility ||
+    thirdPossibility) {
+        console.log("First player won!")
+    } else
+if (fourthPossibility ||
+     fifthPossibility ||
+    sixthPossibility) {
+        console.log("Second player won!")
+    
+ }
+
+ else if (seventhPossibility) {
+     console.log("It's a tie")
+ } else {
+     console.log("Something went wrong!")
+ }
