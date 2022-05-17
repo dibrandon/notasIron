5c25b551662a06470a452f78e623b1f48789d129
dibrandon <diazbrandonalejandro@gmail.com>
2022-05-17 14:38:11 +0200

    switch frutas

--- js/ifElse.js
+++ js/ifElse.js
@@ -92,3 +92,23 @@ else {
 //it will execute the first statement block since the condition is true,
 //finally, it will finish the conditional block, skipping all the following conditions.
 //Once a condition of an if statement is true, it will execute the statements inside {} and skip all the other conditions in the same block.
+
+//  TO SEE HOW THIS EXAMPLE WORKS, UNCOMMENT OUT THE FOLLOWING LINE:
+// also, don't worry about "prompt" that is used below, we will cover it later ;)
+
+const fruit  = prompt ("Enter the fruit name:");
+let type = "";
+
+if (fruit === "pear" || fruit === "apple") {
+  type = "pome";
+} else if (fruit === "peach" || fruit === "coconut") {
+  type = "drupe";
+} else if (fruit === "strawberry" || fruit === "blueberry" || fruit === "blackberry") {
+  type = "berry";
+} else if (fruit === "beans" || fruit === "peas") {
+  type = "legumes";
+} else {
+  type = "other";
+}
+
+console.log(`${fruit} is type of: ${type}.`);
--- js/switch.js
+++ js/switch.js
@@ -1,19 +1,83 @@
-//  TO SEE HOW THIS EXAMPLE WORKS, UNCOMMENT OUT THE FOLLOWING LINE:
-// also, don't worry about "prompt" that is used below, we will cover it later ;)
+//The switch statement evaluates an expression, matching the expression’s value to a case clause, and executes statements (instruction blocks) associated with that case.
 
-const fruit  = prompt ("Enter the fruit name:");
-let type = "";
+switch (expression) {
+    case value1:
+      // statements executed when the result of expression matches value1
+      break;
+    case value2:
+      // statements executed when the result of expression matches value2
+      break;
+    
+    case valueN:
+      // statements executed when the result of expression matches valueN
+      break;
+    default:
+      // statements executed when none of the values match the value of the expression
+      break;
+  }
+  
+  //ejemplo
 
-if (fruit === "pear" || fruit === "apple") {
-  type = "pome";
-} else if (fruit === "peach" || fruit === "coconut") {
-  type = "drupe";
-} else if (fruit === "strawberry" || fruit === "blueberry" || fruit === "blackberry") {
-  type = "berry";
-} else if (fruit === "beans" || fruit === "peas") {
-  type = "legumes";
-} else {
-  type = "other";
+  let pet = "dog";
+
+switch (pet) {
+  case "cat":
+    console.log("I will break a lot of things but hey, I'm adorable ... ")
+    break;
+  case "dog":
+    console.log("I will be a bit annoying but I'm your best friend ... ")
+    break;
+  case "hamster":
+    console.log("I will be impossible to find but I'm reeeeaaaally cute ... ")
+    break;
+  default:
+    console.log("I'm a special pet!")
+    break;
 }
+//To avoid code redundancy in switch statements, you can even write multiple cases to run the same block of code, like shown below:
 
-console.log(`${fruit} is type of: ${type}.`);
+switch (expression) {
+    case value1:
+    case value2:
+      // statements executed when the result of expression matches value1 or value2
+      break;
+    case value3:
+    case value4:
+    case value5:
+      // statements executed when the result of expression matches value3, value4 or value5.
+      break;
+    default:
+      // statements executed when none of the values match the value of the expression
+      break;
+  }
+  
+
+  const fruit  = prompt ("Enter the fruit name:");
+  let type = "";
+  
+  switch (fruit) {
+    case "apple": 
+    case "pear":
+      type = "pome"
+      break;
+      
+    case "peach":
+    case "coconut":
+      type = "drupe"
+      break;
+      
+    case "strawberry":
+    case "bluebery":
+    case "blackberry":
+      type = "berry"
+      break;
+    case "beans":
+    case "peas":
+      type = "legumes"
+      break;
+    default: 
+      type = "other"
+      break;
+  }
+      console.log(`${fruit} is type of: ${type}.`)
+  
