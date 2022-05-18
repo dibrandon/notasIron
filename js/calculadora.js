467051b84491e9d5cc62f233ce834afa630a7151
alejandro_diaz <diazbrandonalejandro@gmail.com>
2022-05-18 13:37:59 +0200

    fin calc function

--- js/calculadora.js
+++ js/calculadora.js
@@ -1,1 +1,42 @@
 // Function Calc
+
+function calc ( symbol,firstNumber, secondNumber) {
+
+    if(symbol === "+"){
+       console.log(firstNumber + secondNumber)
+    }
+    else if (symbol === "-"){
+        console.log(firstNumber - secondNumber)
+
+    }
+else if (symbol === "*"){
+    console.log(firstNumber * secondNumber)
+
+}
+else if (symbol === "/"){
+    console.log(firstNumber / secondNumber)
+
+}
+else{
+    console.log("Error en parametros dados")
+}
+    
+}
+
+calc ("+", 2, 2); // expected 
+
+// calc basica sin IFs
+
+// STEP 1:
+ 
+function calculateSum(num1, num2) {
+    return num1 + num2;
+  }
+   
+  function printResult() {
+    // in the line below, the function result will be stored inside the variable: result
+    const result = calculateSum(3, 6);
+    console.log(`The result is ${result}.`);
+  }
+   
+  printResult();
