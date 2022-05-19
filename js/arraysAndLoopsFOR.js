f68691bb7f03b7d961f171aa76f618af9347ecd7
alejandro_diaz <diazbrandonalejandro@gmail.com>
2022-05-19 21:42:49 +0200

    arrays basis 2, inicio de For

---
+++ js/arraysAndLoopsFOR.js
@@ -0,0 +1,11 @@
+// Arrays and loops
+//One of the most common uses of a loop is to iterate over an array. This means going through the array and being able to do something with each element of the array.
+
+//Let’s look at two ways to print out each element in an array. With both, for and while loops, you will leverage the array’s index to select specific elements.
+
+// "for" loop
+
+
+
+
+
